367b533e1ff4e1e010a7e021a5b625ab3658d7df
Stepanov Ruslan <ruslan.stepanov04@gmail.com>
2024-10-10 18:40:37 +0300

    first kinda working commit

--- app/currencies_api.py
+++ app/currencies_api.py
@@ -5,7 +5,7 @@ API_KEY = "8ad772efe8d44dd6961a3128110e169c"
 
 def get_exchange_rate(base_currency, target_currency):
     # URL для получения актуальных курсов от Open Exchange Rates
-    url = f"https://openexchangerates.org/api/latest.json?app_id={API_KEY}&base={base_currency}"
+    url = f"https://openexchangerates.org/api/latest.json?app_id={API_KEY}&base=USD"
     
     try:
         # Выполняем запрос к API
@@ -17,25 +17,32 @@ def get_exchange_rate(base_currency, target_currency):
 
         # Преобразуем ответ в JSON
         data = response.json()
-
+        
         if response.status_code == 200:
             # Проверяем, есть ли курсы валют в ответе
-            if "rates" in data:
-                rate = data["rates"].get(target_currency)
-                if rate:
-                    return rate
+            if base_currency == "USD":
+                if "rates" in data:
+                    rate = data["rates"].get(target_currency)
+                    if rate:
+                        return rate
+                    else:
+                        return f"Не удалось найти курс для валюты {target_currency}"
                 else:
-                    return f"Не удалось найти курс для валюты {target_currency}"
+                    return "Ошибка: данные о курсах не найдены в ответе API."
             else:
-                return "Ошибка: данные о курсах не найдены в ответе API."
+                if "rates" in data:
+                    rateUSD = data["rates"].get(base_currency)
+                    rate = data["rates"].get(target_currency) / rateUSD
+                    if rate:
+                        return rate
+                    else:
+                        return f"Не удалось найти курс для валюты {target_currency}"
+                else:
+                    return "Ошибка: данные о курсах не найдены в ответе API."
         else:
             return f"Ошибка: {data.get('error', 'Неизвестная ошибка')}"
 
     except Exception as e:
         return f"Ошибка при попытке получить данные: {str(e)}"
+        
 
-# Пример использования
-base_currency = "USD"
-target_currency = "RUB"
-rate = get_exchange_rate(base_currency, target_currency)
-print(f"Курс {base_currency} к {target_currency}: {rate}")
